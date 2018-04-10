ca01358fe32a33b699e1c3bdfd1af789d6d89653
wansh19 <wansh19@ca.com>
2018-04-10 01:02:22 -0700

    removed commented methods

--- src/main/java/manager/SessionHandler.java
+++ src/main/java/manager/SessionHandler.java
@@ -1,46 +1,13 @@
 package manager;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import model.Session;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.http.HttpStatus;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
-import util.Util;
 
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.HttpHeaders;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 
 public class SessionHandler {
     private static final Logger LOGGER = LogManager.getLogger(SessionHandler.class);
-
-//    public static void addNewSession(String appState, String clientState, String clientRedirectUri)
-//            throws Exception {
-//        String session = getSession(clientState, clientRedirectUri);
-//
-//        try {
-//            Response response = ClientBuilder.newClient()
-//                    .target(Util.COOKIE_URL)
-//                    .queryParam(URL_PARAM_NAME, URLEncoder.encode(appState, String.valueOf(StandardCharsets.UTF_8)))
-//                    .queryParam(URL_PARAM_VALUE, URLEncoder.encode(session, String.valueOf(StandardCharsets.UTF_8)))
-//                    .request()
-//                    .post(Entity.json(null));
-//
-//            if(response.getStatus() != HttpStatus.SC_OK){
-//                throw new Exception(response.readEntity(String.class));
-//            }
-//
-//        }catch (Exception e){
-//            throw new Exception(e.getMessage());
-//        }
-//
-//    }
 
     public static String getCookieValue(String name, HttpHeaders headers) throws Exception {
         if (StringUtils.isEmpty(name)) {
@@ -60,26 +27,4 @@ public class SessionHandler {
         }
     }
 
-//    public static String getClientState(String appState) throws Exception {
-//
-//            if(appState == null){
-//                throw new Exception("App State is empty");
-//            }
-//
-//        try {
-//            Response response = ClientBuilder.newClient()
-//                    .target(Util.COOKIE_URL + "/retrieve")
-//                    .queryParam(URL_PARAM_NAME, URLEncoder.encode(appState, String.valueOf(StandardCharsets.UTF_8)))
-//                    .request(MediaType.TEXT_PLAIN)
-//                    .get();
-//
-//            if(response.getStatus() != HttpStatus.SC_OK){
-//                throw new Exception(response.readEntity(String.class));
-//            }
-//
-//            return response.readEntity(String.class);
-//        }catch (Exception e){
-//            throw new Exception(e.getMessage());
-//        }
-//    }
 }
